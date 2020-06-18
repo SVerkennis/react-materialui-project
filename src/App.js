44003cafe2ab36d08ba40be62645369a1d0caf4b
Sabine Verkennis <s.verkennis@yahoo.de>
2020-06-18 12:25:06 +0200

    Add switch router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from 'react';
 import './App.css';
+// import Button from '@material-ui/core/Button';
+import Home from "./components/Home";
+import About from "./components/About";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
+    // <div className="App">
 
-<p>whaaaat</p>
+    <>
+    <header>HEADER</header>
+        <Router>
+            <Switch>
+                <Route exact path="/">
+                    <Home/>
+                </Route>
+                <Route exact path="/about">
+                 <About />
+                </Route>
+            </Switch>
+        </Router>
+    <footer>FOOTER</footer>
+    </>
 
-    </div>
+/*        <Button onClick={() => {window.location="https://www.google.de"} } variant="contained" color="primary" disableElevation>
+            Crazy stuff
+        </Button>*/
+
+   // </div>
+
   );
 }
 
